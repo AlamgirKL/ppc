92289befea204842caea11ff5f7f6708469db9a1
AlamgirKL <alamgirkabir452@gmail.com>
2023-02-23 17:44:39 +0600

    function add

---
+++ basic/function.php
@@ -0,0 +1,16 @@
+<?php 
+// This is called function
+// function show_welcome_user()
+// {
+//     //
+// }
+
+class User
+{
+    // this is called method
+    public function  getUser(){
+        //
+    }
+}
+
+?>
