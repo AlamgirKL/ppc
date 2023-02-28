922be790526b8c5840ac7a38d3d50cbde0323d74
AlamgirKL <alamgirkabir452@gmail.com>
2023-02-28 23:25:20 +0600

    function

--- basic/function.php
+++ basic/function.php
@@ -1,16 +1,77 @@
 <?php 
+declare (strict_types=1 );
+// declare (strict_types = 1);
 // This is called function
 // function show_welcome_user()
 // {
 //     //
 // }
 
-class User
+// $user = new User();// object of User Class 
+// $user -> getUser();
+
+
+// class User
+// {
+//     // this is called method
+//     public function  getUser(){
+//         //
+//     }
+// }
+
+
+// regular function
+// defintion Part
+function show_welcome_user($userName)
 {
-    // this is called method
-    public function  getUser(){
-        //
-    }
+    return "Hi" .$userName."<br/>";
 }
 
-?>
+// calling part
+// $userper = $_GET['username'] ?? 'no one ';
+// echo show_welcome_user ($_GET['username']?? 'no one');// as a parameter , it called argument
+//  echo show_welcome_user($userper);
+
+// echo show_welcome_user ($userper);
+
+echo show_welcome_user($_GET ['username'] ?? 'No one ');
+
+
+echo sum (3,5);
+echo "<br/>";
+
+echo sum(5,8);
+echo "<br/>";
+// echo sum(5, "2");
+
+echo "<br/>";
+
+// echo sum (55 , 'a');
+
+
+function  sum (int $a, int $b) : int
+{
+// function  sum ( $a,  $b){ // reliable 
+
+    var_dump  ($a ,$b);
+    // var_dump ($b);
+  $result = $a + (int)$b; // do the addition 
+
+//   return 'result is : ' .$result ."<br/>";
+
+  return $result;
+}
+
+$totalResult = 0 ;
+
+for ($i = 0 ; $i < 10 ; $i ++ ) {
+    // echo sum ( $i , $i + 2);
+}
+// for ($i = 0 : $i < 10 ; $i++ ){
+//     $totalResult += sum ($i , $i+2);
+// }   
+ 
+$totalResult = $totalResult + sum( $i , $i + 2);
+echo  " Total Result  :  " .$totalResult ;
+
+?>   
