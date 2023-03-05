eccbc426a85cfe8b8d24b597fe4806f0d72ce18a
AlamgirKL <alamgirkabir452@gmail.com>
2023-03-05 23:55:47 +0600

    callback function 39

--- basic/functions.php
+++ basic/functions.php
@@ -6,13 +6,44 @@ declare(  strict_types =1);
 function sum( float $a , float $b, int ...$nums) : float{
     $sum = $a + $b;
 
-    return $sum;
+    return $sum ;
+
+    $string = '';
 
     foreach ($nums as $num){
         $sum += $nums;
+        
     }
 
 
 }
 
-echo sum(33, 11,2,4,5);
+echo sum(33, 11,2,4,5) ."<br/>";
+
+function summation(...$numbers){
+    $string = '';
+
+    foreach( $numbers as $number)
+{
+    $string.= $number;
+}
+
+return $string . "<br/>";
+}
+
+echo summation ( 'abcd','b','d','f','k');
+
+$numberList = [ 12, 13,14,15,16,17, 5,22,11,9];
+$factor = 5 ; 
+
+$multiples = array_filter($numberList , function ($nc) use  ($factor){
+    return $nc % $factor == 0;
+});
+
+echo " Multipules  of $factor\n";
+
+echo "<pre>";
+print_r($multiples);
+echo "</pre>"; // call back function
+
+
