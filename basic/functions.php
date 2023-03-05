d0787e9eedcd3dc1f3a29d0fd9ff1f9bccd9cb1f
AlamgirKL <alamgirkabir452@gmail.com>
2023-03-06 00:39:10 +0600

    anonymous function and String operators

--- basic/functions.php
+++ basic/functions.php
@@ -36,6 +36,8 @@ echo summation ( 'abcd','b','d','f','k');
 $numberList = [ 12, 13,14,15,16,17, 5,22,11,9];
 $factor = 5 ; 
 
+
+
 $multiples = array_filter($numberList , function ($nc) use  ($factor){
     return $nc % $factor == 0;
 });
@@ -50,4 +52,29 @@ $t = " name ";
 $tr = " kabir";
  $t.= $tr;
 
- echo $t;  //String Operators
+ echo $t. "<br/>";  //String Operators
+
+
+ $p = 2;
+ echo " Multipules  of $p\n";
+//  $m = array_filter($numberList , function ($nm) use ($p) {
+//     return $nm % $p == 0 ;
+
+//  }); // anonemus function
+
+
+echo "<pre/>";
+//  print_r($m);
+
+ $m = array_filter($numberList, fn ($np) => $np % $p == 0 );
+
+ print_r($m);
+
+
+ $multi = array_filter( $numberList, 'checkfactor');
+
+
+ function checkfactor($numberw){
+    return $numberw % 3  == 0;
+ }
+ print_r ( $multi);
